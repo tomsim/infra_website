e9fc6a0a0848864362abe6afc13f77f2b66a3936
Ying Wang <ying.wang11@hp.com>
2015-10-20 10:34:11 -0700

    correct syntax error in REST API design
    Change-Id: I73ab0e67a9e4d81aa89efec8bea0af6cf9d0058a

--- src/js/components/DevelopBody.js
+++ src/js/components/DevelopBody.js
@@ -190,7 +190,7 @@ module.exports = function (path, includeMenu) {
                   <span>Web UI</span>
                 </Box>
                 <Box pad={{ horizontal: "small" }}>
-                  <span>REST and Declarative Config</span>
+                  <Link to="documents/user/REST_API_design"><i className="fa fa-angle-right"></i>REST and Declarative Config</Link>
                 </Box>
                 <Box pad={{ horizontal: "small" }}>
                   <span>Zero Touch</span>
@@ -208,7 +208,7 @@ module.exports = function (path, includeMenu) {
             <Box direction="row">
               <Box direction="column" align="start" responsive={true}>
                 <Box pad={{ horizontal: "small" }}>
-		  <Link to="documents/dev/ops-aaa-utils/DESIGN.md"><i className="fa fa-angle-right"></i>AAA</Link>
+		  <span>AAA</span>
                 </Box>
                 <Box pad={{ horizontal: "small" }}>
                   <span>ARP Manager Daemon</span>
