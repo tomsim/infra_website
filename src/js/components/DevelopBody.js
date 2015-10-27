82b7deb386be2f97291330644312fa32fcd1ca1f
Ying Wang <ying.wang11@hp.com>
2015-10-27 10:40:05 -0700

    adding linking document for ops-lldpd design
    Change-Id: I94c30887cbd2fb7dd17910c8304fa295b1db871a

--- src/js/components/DevelopBody.js
+++ src/js/components/DevelopBody.js
@@ -252,7 +252,7 @@ module.exports = function (path, includeMenu) {
                   <Link to="documents/dev/ops-ledd/design"><i className="fa fa-angle-right"></i>LED Daemon</Link>
                 </Box>
                 <Box pad={{ horizontal: "small" }}>
-                  <span>LLDP Daemon</span>
+                  <Link to="documents/dev/ops-lldpd/design"><i className="fa fa-angle-right"></i>LLDP Daemon</Link>
                 </Box>
                 <Box pad={{ horizontal: "small" }}>
                   <span>Management Interface</span>
