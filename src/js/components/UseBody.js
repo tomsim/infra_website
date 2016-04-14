7320d11319a71a8e24344f23ddb55091929be4b2
Rahul Rajagopal <rahulr@hpe.com>
2016-04-14 15:39:06 -0700

    chg: usr: moved thel link for OSPF Cmd reference
    Change-Id: Ia88422e3baa52fea0d153eb48ba91279f56824e6
    Signed-off-by: Rahul Rajagopal <rahulr@hpe.com>

--- src/js/components/UseBody.js
+++ src/js/components/UseBody.js
@@ -184,9 +184,6 @@ module.exports = function (path, includeMenu) {
                   <Box pad={{ horizontal: "small" }}>
                     <Link to="/documents/user/dhcp_tftp_cli"><i className="fa fa-angle-right"></i>DHCP/TFTP Server</Link>
                   </Box>
-                  <Box pad={{ horizontal: "small" }}>
-                    <Link to="/documents/user/ospfv2_cli"><i className="fa fa-angle-right"></i>OSPF</Link>
-                  </Box>
                 </Box>
                 <Box direction="column" align="start" responsive={true}>
                   <Box pad={{ horizontal: "small" }}>
@@ -242,6 +239,9 @@ module.exports = function (path, includeMenu) {
                 <Box direction="column" align="start" responsive={true}>
                   <Box pad={{ horizontal: "small" }}>
                     <Link to="/documents/user/autoprovision_CLI"><i className="fa fa-angle-right"></i>Zero Touch</Link>
+                  </Box>
+                  <Box pad={{ horizontal: "small" }}>
+                    <Link to="/documents/user/ospfv2_cli"><i className="fa fa-angle-right"></i>OSPF</Link>
                   </Box>
                 </Box>
                 </Box>
