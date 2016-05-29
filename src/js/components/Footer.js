54525359831c34b78a6574c6bece7c1e036d3503
Diego Dompe <dompe@hpe.com>
2016-05-28 20:00:38 -0600

    Correct Copyright
    Change-Id: I41d2a100e98227cbf8991bd8c3ae2bc199e06112
    Signed-off-by: Diego Dompe <dompe@hpe.com>

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -16,7 +16,8 @@ var Footer = React.createClass({
     <div id="lfcollabprojects-footer">
       <div class="gray-diagonal">
         <div class="footer-inner">
-          <p>&copy; 2015 - 2016 Linux Foundation, HPE and others. All Rights Reserved.</p>
+          <p>&copy; 2016 Linux Foundation</p>
+          <p>&copy; 2015-2016 Hewlett Packard Enterprise Development LP</p>
           <p>Linux Foundation is a registered trademark of The Linux Foundation.</p>
           <p>Linux is a registered <a href="http://www.linuxfoundation.org/programs/legal/trademark" title="Linux Mark Institute">trademark</a> of Linus Torvalds.</p>
           <p><a href="http://www8.hp.com/us/en/hpe/legal/terms-of-use.html" className="scroll">Terms of use</a> | <a href="https://www.hpe.com/us/en/legal/privacy.html" className="scroll">Privacy</a></p>
