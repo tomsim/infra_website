2648c69289138cd62f066c80c031510613d1b997
Cliff Wichmann <wichmann.ca@gmail.com>
2017-02-17 12:30:32 -0800

    Adding dellemc copyright
    Change-Id: I6a579ffdc298acf7b54ca55bb8a179c23bdac31f

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -18,6 +18,7 @@ var Footer = React.createClass({
         <div class="footer-inner">
           <p>&copy; 2016 Linux Foundation</p>
           <p>&copy; 2015-2016 Hewlett Packard Enterprise Development LP</p>
+          <p>&copy; 2016-2017 DellEMC</p>
           <p>Linux Foundation is a registered trademark of The Linux Foundation.</p>
           <p>Linux is a registered <a href="http://www.linuxfoundation.org/programs/legal/trademark" title="Linux Mark Institute">trademark</a> of Linus Torvalds.</p>
           <p><a href="http://www8.hp.com/us/en/hpe/legal/terms-of-use.html" className="scroll">Terms of use</a> | <a href="https://www.hpe.com/us/en/legal/privacy.html" className="scroll">Privacy</a></p>
